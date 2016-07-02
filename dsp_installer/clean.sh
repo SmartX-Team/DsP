f7a60a938e29501013c3e593c244010874a066de
Jun-Sik Shin <jsshin@smartx.kr>
2016-07-02 17:50:13 +0900

    Initialize the branch to program from the scratch

--- dsp_installer/clean.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-TARGET_PATH="${HOME}/dsp_installer/dsp_installer"
-
-cd ${TARGET_PATH}
-rm -f *_ubuntu*.sh *_openstack*.sh local.conf* *wsgi-keystone* *client.py
-sudo rm -f /home/tein/dsp_installer/maasrepo/scripts/* 
--- installer_inventory/ubuntu_installer/ubuntu_add_cfg.sh
+++
@@ -1,7 +0,0 @@
-
-echo "#!/bin/bash -e" > /etc/rc.local
-echo "exit 0" >> /etc/rc.local
-date >> /root/test_time
-##################  ubuntu_add_cfg.sh  #############################
-####################################################################
-
