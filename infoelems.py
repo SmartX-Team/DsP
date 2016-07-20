ee36b1d388350e3fe78fb32732b3004f9322f7c6
Jun-Sik Shin <jsshin@smartx.kr>
2016-07-20 17:13:19 +0900

    Add get/set methods inside infoelems.py file

--- infoelems.py
+++ infoelems.py
@@ -1,26 +1,37 @@
 class BoxInfo:
     def __init__(self):
-        self.name = ""
-        self.type = ""
-        self.sw = list()
+        self._name = ""
+        self._type = ""
+        self._sw = list()
 
     def getname(self):
-        return self.name
+        return self._name
+
+    def setname(self, name):
+        self._name = name
 
     def gettype(self):
-        return self.type
+        return self._type
+
+    def settype(self, type):
+        self._type = type
 
     def getswinfo(self):
-        return self.sw
+        return self._sw
 
+    def setswinfo(self, swinfo):
+        self._sw.append(swinfo)
 
 class SwInfo:
     def __init__(self):
-        self.name = ""
-        self.params = dict()
+        self._name = ""
+        self._params = dict()
 
     def getname(self):
-        return self.name
+        return self._name
 
     def getparams(self):
-        return self.params
+        return self._params
+
+    def setparam(self, k, v):
+        self._params[k] = v
