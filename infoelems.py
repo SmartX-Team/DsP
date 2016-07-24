79b50412e8f5d1c89bd7c34bad4e63cba9427804
Jun-Sik Shin <jsshin@smartx.kr>
2016-07-24 14:34:15 +0900

    Add account variables in BoxInfo class

--- infoelems.py
+++ infoelems.py
@@ -5,6 +5,8 @@ class BoxInfo:
     def __init__(self):
         self.name = ""
         self.type = ""
+        self.accid = ""
+        self.accpw = ""
         self.sw = list()
         self.nic = list()
 
