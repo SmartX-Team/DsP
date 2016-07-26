14c1f6a47f913486df4f331415ef50ac9e50bc8a
gist-koko <power8723@gmail.com>
2016-07-26 15:43:36 +0900

    Input value modified from list(BoxInfo) to str(software_name)

--- inven/prepare_supervisor.py
+++ inven/prepare_supervisor.py
@@ -22,93 +22,66 @@ print type(a_list) is list
 
 """
 prepare (boxlist):
-	input value : list(BoxInfo)
-	with BoxInfo, filled with swname, swtype.
+   input value : list(BoxInfo)
+   with BoxInfo, filled with swname, swtype.
 """
-def prepare(boxlist):
+def prepare(swname, swtype):
 
-	installer_path = os.getcwd()
-	ret_list = []
-	if type(boxlist) is list:
-		for box in boxlist:
-			for sw in box.getswinfo():
-				swname = sw.getname()
-				swpath = path.join(installer_path,'inven', swname)
-				swtype = sw.gettype()
-	
-				try:
-					f = open(swpath + '/setting.yaml',"r")
-					sw_setting = f.read()
-	
-				except IOError:
-					print "Setting file IOError"\
-							"\nIs it in %s?"%swpath
-					exit(-1)
-				#YAML file load successful
-				#Find setting for swtype in yaml configuration file
-				default_config = [i for i in yaml.load_all(sw_setting) if i['swtype']=='Default']
-				sw_config = [i for i in yaml.load_all(sw_setting) if i['swtype']==swtype]
-				if len(sw_config) == 1: #one unique setting for swtype in YAML exists
-					print "Found : setting for %s:%s"%(swname,swtype)
-					for par in default_config[0]['Parameter']:
-						sw.setparam(par,default_config[0]['Parameter'][par])
-					for par in sw_config[0]['Parameter']:
-						sw.setparam(par,sw_config[0]['Parameter'][par])
-	#				print sw.getparams()
-	
-				elif len(config) == 0: #No setting for swtype
-					print "Not Found : setting for %s:%s"%(swname, swtype)
-					exit(-1)
-				else: #multiple setting for swtype in YAML exists
-					print "Error : More than one setting exists for %s:%s"%(swname, swtype) 
-					exit(-1)
-		return boxlist
-	elif isinstance (boxlist,infoelems.BoxInfo):
-		print 'TBI - currently, only works for list(BoxInfo)'
-	else:
-		print "%s in wrong data type - not BoxInfo nor list(BoxInfo)"%boxlist
-		print type(boxlist)
-		
-	
+   sw = infoelems.SwInfo()
+   installer_path = os.getcwd()
+   swpath = path.join(installer_path,'inven', swname)
+   
+   try:
+      f = open(swpath + '/setting.yaml',"r")
+      sw_setting = f.read()
+   
+   except IOError:
+      print "Setting file IOError"\
+            "\nIs it in %s?"%swpath
+      exit(-1)
+   #YAML file load successful
+   #Find setting for swtype in yaml configuration file
+   default_config = [i for i in yaml.load_all(sw_setting) if i['swtype']=='Default']
+   sw_config = [i for i in yaml.load_all(sw_setting) if i['swtype']==swtype]
+   if len(sw_config) == 1: #one unique setting for swtype in YAML exists
+      print "Found : setting for %s:%s"%(swname,swtype)
+      for par in default_config[0]['Parameter']:
+         sw.params[par] = default_config[0]['Parameter'][par]
+      for par in sw_config[0]['Parameter']:
+         sw.params[par] = sw_config[0]['Parameter'][par]
+#           print sw.getparams()
+ 
+   elif len(config) == 0: #No setting for swtype
+      print "Not Found : setting for %s:%s"%(swname, swtype)
+      exit(-1)
+   else: #multiple setting for swtype in YAML exists
+      print "Error : More than one setting exists for %s:%s"%(swname, swtype) 
+      exit(-1)
+   return sw
+      
+   
 
 
 
-def verify(boxlist):
+def verify(swname):
+   installer_path = os.getcwd()
+   inven_dir   = path.join(installer_path,'inven')
+   try:
+      f = open(inven_dir + "/AVAILABLE_SOFTWARE",'r')
+   except IOError:
+      print "Failed to open AVAILABLE_SOFTWARE "\
+            "\nIs it in %s?"%inven_dir 
+      exit(-1)
 
-	"""
-	Currently, location of AVAILABLE_SOFTWARE is 
-	$(current directory)/
-	"""
-	installer_path = os.getcwd()
-	inven_dir	= path.join(installer_path,'inven')
-	try:
-		f = open(inven_dir + "/AVAILABLE_SOFTWARE",'r')
-	except IOError:
-		print "Failed to open AVAILABLE_SOFTWARE "\
-				"\nIs it in %s?"%inven_dir	
-		exit(-1)
-
-	sw_list = f.read().splitlines()
-	print "SW list : "+ ', '.join(sw_list)
-
-	if type(boxlist) is list:
-		for box in boxlist:
-			for boxsw in box.getswinfo():
-				if boxsw.getname() not in sw_list:
-					print "Software %s is not available"%boxsw
-					exit(-1)
-	
-	elif isinstance (boxlist,infoelems.BoxInfo):
-		for boxsw in boxlist.getswinfo():
-			if boxsw.getname() not in sw_list:
-				print "Software %s is not available"%boxsw
-				exit(-1)
-	
-	else:
-		print "%s in wrong data type - not BoxInfo nor list(BoxInfo)"%boxlist
-		print type(boxlist)
-		exit(-1)
-	
-	# Out of loop : All software available
-	print boxlist.getswinfo()[0]._param
-	print "All available"
+   sw_list = f.read().splitlines()
+   if type(swname) is str:
+      if swname not in sw_list:
+         print "Software %s is not available"%swname
+         exit(-1)
+   else:
+      print "%s in wrong data type - String required"%boxlist
+      print type(boxlist)
+      exit(-1)
+   
+   # Out of loop : All software available
+   print "Software available"
