95c64268fd38302461debb375b915e7fa12c6c71
Jun-Sik Shin <jsshin@smartx.kr>
2017-12-28 14:04:58 +0100

    Simplified StoreManager (Prev SecuredRepoMgr) Class

--- repo/repo.py
+++ repo/repo.py
@@ -10,7 +10,7 @@ from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
 import logging
 
 
-class SecuredRepoMgr(object):
+class StoreManager(object):
     """
     Secured Repository Manager
     This class provides secured management of configuration files for
@@ -21,29 +21,21 @@ class SecuredRepoMgr(object):
 
     def __new__(cls, *args, **kwargs):
         if not cls._instance:
-            cls._instance = super(SecuredRepoMgr, cls).__new__(cls)
+            cls._instance = super(StoreManager, cls).__new__(cls)
         return cls._instance
 
-    def __init__(self):
-        self._seckey = None
-        self._secseed = None
-        self._secure_mode = None
+    def __init__(self, secure_mode=False):
+        self._security_mode = secure_mode
+        self._security_key = None
+        self._logger = logging.getLogger(self.__class__.__name__)
 
-        self._logger = logging.getLogger("SecuredRepositoryManager")
-        self._logger.setLevel(logging.DEBUG)
-        fm = logging.Formatter('%(name)-12s: %(levelname)-8s %(message)s')
-        ch = logging.StreamHandler()
-        ch.setLevel(logging.DEBUG)
-        ch.setFormatter(fm)
-        self._logger.addHandler(ch)
+    def init_secure_key(self, salt):
+        if self._security_mode:
+            self._load_seckey(salt)
+        else:
+            self._logger.warn("This Repo Manager is working on NON-SECURE Mode")
 
-    def initialize(self, __salt, __is_secure=False):
-        if __is_secure:
-            self.load_seckey(__salt)
-            self._secure_mode = __is_secure
-
-    def load_seckey(self, salt):
-        self._secseed = salt
+    def _load_seckey(self, salt):
         kdf = PBKDF2HMAC(
             algorithm=hashes.SHA256,
             length=32,
@@ -52,48 +44,37 @@ class SecuredRepoMgr(object):
             backend=default_backend()
         )
         p = input("Input the password for secured repository: ")
-        self._seckey = base64.urlsafe_b64encode(kdf.derive(p))
+        self._security_key = base64.urlsafe_b64encode(kdf.derive(p))
 
-    def read_file(self, __file_path):
-        if self._secure_mode:
+    def read_file(self, filename):
+        filepath = os.path.join(os.getcwd(), filename)
+
+        file = open(filepath, 'r')
+        contents = file.read(-1)
+        file.close()
+
+        if self._security_mode:
             self._logger.debug("Enter the read_file with secure mode")
-            return self._read_encrypt_file(__file_path)
-        else:
-            rf = open(__file_path, 'r')
-            l = rf.read(-1)
-            rf.close()
-            return l
+            f = Fernet(self._security_key)
+            try:
+                contents = f.decrypt(contents)
+            except cryptography.fernet.InvalidToken:
+                self._logger.error("Password or Salt is not correct.")
 
-    def _read_encrypt_file(self, __encrypt_file_path):
-        rf = open(__encrypt_file_path, 'r')
-        l = rf.read(-1)
-        rf.close()
-        f = Fernet(self._seckey)
-        try:
-            return f.decrypt(l)
-        except cryptography.fernet.InvalidToken:
-            self._logger.error("Password or Salt is not correct.")
-            return None
+        return contents
 
-    def _write_encrypt_file(self, __encrypt_file_path, __plain_text):
-        wf = open(__encrypt_file_path, 'w')
-        f = Fernet(self._seckey)
-        wf.write((f.encrypt(__plain_text)))
-        wf.close()
+    def write_file(self, filename, contents):
+        filepath = os.path.join(os.getcwd(), filename)
 
-    def encrypt_file(self, org_file_path, enc_file_path):
-        rf = open(org_file_path, 'r')
-        l = rf.read(-1)
-        rf.close()
-        self._write_encrypt_file(enc_file_path, l)
-
-    def decrypt_file(self, enc_file_path, dec_file_path):
-        wf = open(dec_file_path, 'w')
-        wf.write(self._read_encrypt_file(enc_file_path))
-        wf.close()
+        if self._security_mode:
+            f = Fernet(self._security_key)
+            contents = f.encrypt(contents)
+        file = open(filepath, 'w')
+        file.write(contents)
+        file.close()
 
 
 if __name__ == "__main__":
-    sr = SecuredRepoMgr()
+    sr = StoreManager()
     ts = os.urandom(16)
     sr.load_seckey(ts)
