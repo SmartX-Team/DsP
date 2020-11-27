ba72b1e9c752fa003ceb5c3d9b4efc8e5e64796f
Jun-Sik Shin <jun@Jun-NetAI-MacBook.local>
2020-11-27 15:00:19 +0900

    Refactor measurement_flow.c to collect flows

--- post/inventory/ansible/playbook/function/multi-sec/flow_measure/measurement_flow.c
+++ post/inventory/ansible/playbook/function/multi-sec/flow_measure/measurement_flow.c
@@ -23,39 +23,48 @@ struct flow_tuple_t {
 };
 
 struct flow_stat_t{
-    u32 start_time;
-    u32 end_time;
+    u64 start_ts;
+    u64 last_ts;
+    //
+    u32 pkt_cnt;
+    u16 pkt_bytes_min;
+    u16 pkt_bytes_max;
+    u32 pkt_bytes_total;
+    //
+    u32 ipat_min;    // ipat = inter-packet arrival time
+    u32 ipat_max;
+    u64 ipat_total;
+    //
+    u16 tcp_syn_cnt;
+    u16 tcp_ack_cnt;
+    u16 tcp_fin_cnt;
+    u16 tcp_rst_cnt;
+    u16 tcp_psh_cnt;
+    u16 tcp_urg_cnt;
 
-    u16 spkt_cnt;
-    u16 dpkt_cnt;
-    u16 spkt_byte;
-    u16 dpkt_byte;
-
-    u16 syn_cnt;
-    u16 ack_cnt;
-    u16 rst_cnt;
-    u16 fin_cnt;
+//    u32 tcp_cwr_cnt;
+//    u32 tcp_ece_cnt;
 };
 
-BPF_PERF_OUTPUT(skb_events);    // has to be delcared outside any function
-BPF_HASH(<map_name>, struct flow_key_t, struct flow_info_t); // let's try to save the number of IPs in here
+BPF_HASH(<map_name>, struct flow_tuple_t, struct flow_stat_t); // let's try to save the number of IPs in here
 
 int packet_measure(struct __sk_buff *skb) {
+    u64 cur_time = bpf_ktime_get_ns();
+//    bpf_trace_printk("%llu => Start packet_measure() \n", cur_time);
     u8 *cursor = 0;
 
     struct flow_tuple_t flow_tuple = {0};
-    struct flow_stat_t *flow_stat;
 
     u32 pkt_len = 0;
-    u8 tcp_cwr, tcp_ece, tcp_urg, tcp_ack, tcp_psh, tcp_rst, tcp_syn, tcp_fin;
+    u8 tcp_cwr = 0, tcp_ece = 0, tcp_urg = 0, tcp_ack = 0, tcp_psh = 0, tcp_rst = 0, tcp_syn = 0, tcp_fin = 0;
 
     // Other metrics
     // Current time
     // Packet length
 
     ETH: {
+//        bpf_trace_printk("ETH \n");
         struct ethernet_t *ethernet = cursor_advance(cursor, sizeof(*ethernet));  // ethernet header (frame)
-        payload_offset += ETH_HLEN;
         pkt_len += ETH_HLEN;
 
         //filter IP packets (ethernet type = 0x0800) 0x0800 is IPv4 packet
@@ -67,14 +76,15 @@ int packet_measure(struct __sk_buff *skb) {
     }
 
     DOT1Q: {
+//        bpf_trace_printk("DOT1Q \n");
         struct dot1q_t *dot1q = cursor_advance(cursor, sizeof(*dot1q));
-        payload_offset += DOT1Q_HLEN;
         pkt_len += DOT1Q_HLEN;
 
         goto IPV4;
     }
 
     IPV4: {
+//        bpf_trace_printk("IPV4 \n");
         struct ip_t *ip = cursor_advance(cursor, sizeof(*ip));	// IP header (datagram)
         /*
           calculate ip header length
@@ -97,16 +107,14 @@ int packet_measure(struct __sk_buff *skb) {
     }
 
     ICMP4: {
+//        bpf_trace_printk("ICMP4 \n");
         //  This version does not handle ICMP4 headers
-        payload_offset += ICMP4_HLEN;
-        payload_length -= ICMP4_HLEN;
-        goto STORE;
+        goto DROP;
     }
 
     UDP: {
+//        bpf_trace_printk("UDP \n");
         struct udp_t *udp = cursor_advance(cursor, sizeof(*udp));
-        payload_offset += UDP_HLEN;
-        payload_length -= UDP_HLEN;
 
         flow_tuple.dport = udp -> dport;
         flow_tuple.sport = udp -> sport;
@@ -122,8 +130,6 @@ int packet_measure(struct __sk_buff *skb) {
           e.g. tcp->offset = 5 ; TCP Header Length = 5 x 4 byte = 20 byte
           The minimum value for this field is 5, which indicates a length of 5 x 32 bits(4 bytes) = 20 bytes
         */
-        payload_offset += tcp -> offset << 2; //SHL 2 -> *4 multiply
-        payload_length -= tcp -> offset << 2;
 
         flow_tuple.dport = tcp -> dst_port;
         flow_tuple.sport = tcp -> src_port;
@@ -137,48 +143,89 @@ int packet_measure(struct __sk_buff *skb) {
         tcp_syn = tcp -> flag_syn;
         tcp_fin = tcp -> flag_fin;
 
+//        bpf_trace_printk("TCP %d\n", tcp_syn);
+
         goto STORE;
     }
 
 	STORE: {
-	    struct flow_tuple_t rev_flow_tuple = {
-	        dip = flow_tuple -> dip;
-            sip = flow_tuple -> sip;
-            dport = flow_tuple -> dport;
-            sport = flow_tuple -> sport;
-            l4_proto = flow_tuple -> l4_proto;
-	    };
+//	    bpf_trace_printk("STORE \n");
+	    // Look up the map to find a flow entry having same tuples
 
-	    rev_flow_stat = <map_name>.lookup(&flow_tuple);
-	    if(rev_flow_stat) {
-	        return 0;
-	    }
-
-	    flow_stat = <map_name>.lookup(&flow_tuple); // this prevents transmitted packets from being counted
+	    struct flow_stat_t *flow_stat = <map_name>.lookup(&flow_tuple); // this prevents transmitted packets from being counted
 	    if (flow_stat) {
-	        flow_stat->flow_cnt += 1;
-	        flow_stat->flow_bytes += pkt_len;
-	    } else {
-	        struct flow_stat_t tmp_flow_stat = {.flow_cnt = 1, .flow_bytes = pkt_len};
-	        <map_name>.update(&flow_tuple, &tmp_flow_stat);
-	    }
+	        bpf_trace_printk("Found a matched flow from the map \n");
+            u64 cur_ipat = flow_stat->last_ts - cur_time;
+
+            flow_stat->last_ts = cur_time;
+            flow_stat->pkt_cnt += 1;
+
+            if(pkt_len < flow_stat->pkt_bytes_min) flow_stat->pkt_bytes_min = pkt_len;
+            if(pkt_len > flow_stat->pkt_bytes_max) flow_stat->pkt_bytes_max = pkt_len;
+            flow_stat->pkt_bytes_total += pkt_len;
+
+            if (cur_ipat < flow_stat->ipat_min) flow_stat->ipat_min = cur_ipat;
+            if (cur_ipat > flow_stat->ipat_max) flow_stat->ipat_max = cur_ipat;
+            flow_stat->ipat_total += cur_ipat;
+
+            if (tcp_syn) flow_stat->tcp_syn_cnt += 1;
+            if (tcp_ack) flow_stat->tcp_ack_cnt += 1;
+            if (tcp_fin) flow_stat->tcp_fin_cnt += 1;
+            if (tcp_rst) flow_stat->tcp_rst_cnt += 1;
+            if (tcp_psh) flow_stat->tcp_psh_cnt += 1;
+            if (tcp_urg) flow_stat->tcp_urg_cnt += 1;
+
+            goto EOP;
+        } else {
+            bpf_trace_printk("Cannot found a matched flow from the map \n");
+            struct flow_stat_t new_flow_stat = {0};
+            new_flow_stat.start_ts = cur_time;
+            new_flow_stat.last_ts = cur_time;
+            new_flow_stat.pkt_cnt = 0;
+            new_flow_stat.pkt_bytes_min = pkt_len;
+            new_flow_stat.pkt_bytes_max = pkt_len;
+            new_flow_stat.pkt_bytes_total = pkt_len;
+            new_flow_stat.ipat_min = 0;
+            new_flow_stat.ipat_max = 0;
+            new_flow_stat.ipat_total = 0;
+            new_flow_stat.tcp_syn_cnt = tcp_syn;
+            new_flow_stat.tcp_ack_cnt = tcp_ack;
+            new_flow_stat.tcp_fin_cnt = tcp_fin;
+            new_flow_stat.tcp_rst_cnt = tcp_rst;
+            new_flow_stat.tcp_psh_cnt = tcp_psh;
+            new_flow_stat.tcp_urg_cnt = tcp_urg;
+
+            <map_name>.insert(&flow_tuple, &new_flow_stat);
+            goto EOP;
+        }
     }
 
+    EOP:
+//        bpf_trace_printk("EOP \n\n");
+        return 0;
+
 	DROP:
+//	    bpf_trace_printk("DROP \n\n");
         return -1;
 }
 
 //struct flow_stat_t{
-//    u32 start_time;
-//    u32 end_time;
+//    u32 start_ts;
+//    u32 last_ts;
 //
-//    u16 spkt_cnt;
-//    u16 dpkt_cnt;
-//    u16 spkt_byte;
-//    u16 dpkt_byte;
+//    u32 pkt_cnt;
+//    u16 pkt_bytes_min;
+//    u16 pkt_bytes_max;
+//    u32 pkt_bytes_total;
 //
-//    u16 syn_cnt;
-//    u16 ack_cnt;
-//    u16 rst_cnt;
-//    u16 fin_cnt;
+//    u32 ipat_min;    // ipat = inter-packet arrival time
+//    u32 ipat_max;
+//    u32 ipat_total;
+//
+//    u32 tcp_syn_cnt;
+//    u32 tcp_ack_cnt;
+//    u32 tcp_fin_cnt;
+//    u32 tcp_rst_cnt;
+//    u32 tcp_psh_cnt;
+//    u32 tcp_urg_cnt;
 //};
